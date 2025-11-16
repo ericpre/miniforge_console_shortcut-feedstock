10838a2120a5f15449a24e8e890010425ed2e5b7
Eric Prestat <eric.prestat@gmail.com>
2025-11-16 14:30:11 +0000

    Rename `console_shortcut` to `miniforge_console_shortcut` for consistency with package name and the menu name used in the `menuinst` command line

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -3,8 +3,8 @@ SET "MENU_DIR=%PREFIX%\Menu"
 IF NOT EXIST "%MENU_DIR%" MKDIR "%MENU_DIR%"
 
 :: icon is in public domain: https://github.com/paomedia/small-n-flat
-copy "%RECIPE_DIR%\console_shortcut.ico" %MENU_DIR%
+copy "%RECIPE_DIR%\miniforge_console_shortcut.ico" %MENU_DIR%
 IF ERRORLEVEL 1 EXIT /B 1
 
-COPY "%RECIPE_DIR%\menu.json" "%MENU_DIR%\console_shortcut_menu.json"
+COPY "%RECIPE_DIR%\menu.json" "%MENU_DIR%\miniforge_console_shortcut_menu.json"
 IF ERRORLEVEL 1 EXIT /B 1
